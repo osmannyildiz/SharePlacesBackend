8c220d9a790cb8c77492f4c6ab2073872375feac
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-04-01 00:28:50 +0300

    92-end

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/users", usersRouter);
 app.use("/api/places", placesRouter);
 
+app.use((error, req, res, next) => {
+	if (res.headerSent) {
+		return next(error);
+	}
+
+	res.status(error.statusCode || 500);
+	res.json({
+		ok: false,
+		message: error.message || "An unknown error occurred.",
+	});
+});
+
 app.listen(5000);
--- routes/places.js
+++ routes/places.js
@@ -4,7 +4,7 @@ import { PLACES } from "../dummyData.js";
 const placesRouter = express.Router();
 
 // Get places (filter by user id)
-placesRouter.get("/", (req, res) => {
+placesRouter.get("/", (req, res, next) => {
 	const { userId } = req.query;
 
 	let places = PLACES;
@@ -13,20 +13,28 @@ placesRouter.get("/", (req, res) => {
 		places = places.filter((place) => place.userId === parseInt(userId));
 	}
 
-	res.json({ ok: true, data: places });
+	// if (!places) {  // Doesn't work
+	if (!places.length) {
+		const error = new Error("No place found.");
+		error.statusCode = 404;
+		return next(error);
+	}
+
+	return res.json({ ok: true, data: places });
 });
 
 // Get place by id
-placesRouter.get("/:id", (req, res) => {
+placesRouter.get("/:id", (req, res, next) => {
 	const { id } = req.params;
 
 	const place = PLACES.find((place) => place.id === parseInt(id));
 	if (!place) {
-		// TODO Return 404
-		return res.json({ ok: false, message: "This place doesn't exist." });
+		const error = new Error("This place doesn't exist.");
+		error.statusCode = 404;
+		return next(error);
 	}
 
-	res.json({ ok: true, data: place });
+	return res.json({ ok: true, data: place });
 });
 
 export default placesRouter;
