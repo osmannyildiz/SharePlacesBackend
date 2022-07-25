825dbf05ff1e6b89748c868520f60fecd34001f7
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-07-25 20:36:22 +0300

    Fix: Symlink didn't work

---
+++ public/precache-manifest.be6c3d466466fa7ce05f770841a220dc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04eb1721de4371286b0c4a51eab49a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d5dd23a8c5dbb2972a6",
+    "url": "/static/css/0.d8a44fbd.chunk.css"
+  },
+  {
+    "revision": "5496684352eeafe497f5",
+    "url": "/static/css/4.c273e94a.chunk.css"
+  },
+  {
+    "revision": "8eecf34812f550550425",
+    "url": "/static/css/5.ab4b7e64.chunk.css"
+  },
+  {
+    "revision": "90193511c0f9c0c0b378",
+    "url": "/static/css/6.965ce428.chunk.css"
+  },
+  {
+    "revision": "b1c5444bbda168f9cab4",
+    "url": "/static/css/7.8aa08c5b.chunk.css"
+  },
+  {
+    "revision": "dc62e9ce5a7757e10d11",
+    "url": "/static/css/8.e2599b16.chunk.css"
+  },
+  {
+    "revision": "9093bc014a81ecb83acf",
+    "url": "/static/css/main.d727eace.chunk.css"
+  },
+  {
+    "revision": "2d5dd23a8c5dbb2972a6",
+    "url": "/static/js/0.524d991c.chunk.js"
+  },
+  {
+    "revision": "44edfa741745df88143c",
+    "url": "/static/js/3.bca4b66b.chunk.js"
+  },
+  {
+    "revision": "5496684352eeafe497f5",
+    "url": "/static/js/4.a6297e02.chunk.js"
+  },
+  {
+    "revision": "8eecf34812f550550425",
+    "url": "/static/js/5.612b9b30.chunk.js"
+  },
+  {
+    "revision": "90193511c0f9c0c0b378",
+    "url": "/static/js/6.83645546.chunk.js"
+  },
+  {
+    "revision": "b1c5444bbda168f9cab4",
+    "url": "/static/js/7.4c4ffb95.chunk.js"
+  },
+  {
+    "revision": "dc62e9ce5a7757e10d11",
+    "url": "/static/js/8.35d9aa6d.chunk.js"
+  },
+  {
+    "revision": "9093bc014a81ecb83acf",
+    "url": "/static/js/main.3574463b.chunk.js"
+  },
+  {
+    "revision": "e5e1f2244376dca777ef",
+    "url": "/static/js/runtime-main.34fef886.js"
+  }
+]);
